851e5dc4b37b4b7ccaa2b05626c9c0538123f52f
Marvin H. Zavala <hazielzs@hotmail.com>
2025-09-29 14:09:19 -0600

    feat: first commit to deploy

--- server.js
+++ server.js
@@ -9,11 +9,21 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+app.use(expressLayouts)
+//Index Route
+app.get("/", (req, res) => {
+  res.render("index", {title: "Home"})
+})
+
+app.set("view engine", "ejs")
+app.set("layout", "./layouts/main")
+app.set("layout","./layouts/layout")
 
 /* ***********************
  * Local Server Information
